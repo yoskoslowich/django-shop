bf57419a5e9942f2b236822987af8074a7a087b9
Yoskoslowich Fernandes <yoskos@outlook.com>
2023-07-05 13:55:52 -0300

    insert bug

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print("var: %d" % 'string')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_shop.settings')
     try:
         from django.core.management import execute_from_command_line
